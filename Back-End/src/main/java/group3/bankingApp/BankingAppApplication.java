cb83c6d9f02f45bdc87f029eb13e76ac0f28a95f
721688 <721688@student.inholland.nl>
2025-04-23 15:42:21 +0200

    added route

--- Back-End/src/main/java/group3/bankingApp/BankingAppApplication.java
+++ Back-End/src/main/java/group3/bankingApp/BankingAppApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @SpringBootApplication
 @RestController
@@ -19,4 +18,11 @@ public class BankingAppApplication {
     public String hello(@RequestParam(value = "name", defaultValue = "BANANA") String name) {
       return String.format("ROBBEN SUCK MY %s!", name);
     }
+	
+	@GetMapping("/customer/{id:[0-9]+}/account")
+	public String getAccounts(@PathVariable("id") String id) {
+    return id;
+	}
+	
 }
+
