13c2faa35b396211113576046e482ff0442adbc2
721688 <721688@student.inholland.nl>
2025-04-23 11:29:32 +0200

    /Back-End spring boot bootstrap

---
+++ Back-End/src/test/java/group3/bankingApp/BankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package group3.bankingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
