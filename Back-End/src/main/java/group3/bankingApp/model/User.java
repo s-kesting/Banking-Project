18c0798b6b2c8ea7b9aa77e6af0650d0771ec274
ZFengLiang <712789@student.inholland.nl>
2025-05-06 16:05:39 +0200

    please

---
+++ Back-End/src/main/java/group3/bankingApp/model/User.java
@@ -0,0 +1,96 @@
+package group3.bankingApp.model;
+
+import group3.bankingApp.model.enums.Role;
+import group3.bankingApp.model.enums.VerifyStatus;
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false, length = 20)
+    private String phoneNumber;    
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String bsn;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "verify_status", nullable = false)
+    private VerifyStatus verifyUser;
+
+    // -- Constructors, getters & setters --
+
+    public Integer getUserId() {
+        return userId;
+    }
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getBsn() {
+        return bsn;
+    }
+    public void setBsn(String bsn) {
+        this.bsn = bsn;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public VerifyStatus getVerifyUser() {
+        return verifyUser;
+    }
+    public void setVerifyUser(VerifyStatus verifyUser) {
+        this.verifyUser = verifyUser;
+    }
+}
---
+++ Back-End/src/main/java/group3/bankingApp/services/BankingAppApplication.java
@@ -0,0 +1,31 @@
+package group3.bankingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class BankingAppApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingAppApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "BANANA") String name) {
+		return String.format("ROBBEN SUCK MY %s!", name);
+	}
+
+	@GetMapping("/customer/{id:[0-9]+}/account")
+	public String getAccounts(@PathVariable("id") String id) {
+		return id;
+	}
+
+}
+
