061eb4ebb43f9ca465a8df48aa148bc61bb125aa
721688 <721688@student.inholland.nl>
2025-05-20 12:15:36 +0200

    fixed minor oversight

--- Back-End/src/main/java/group3/bankingApp/controller/AccountController.java
+++ Back-End/src/main/java/group3/bankingApp/controller/AccountController.java
@@ -3,14 +3,8 @@ package group3.bankingApp.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import group3.bankingApp.services.AccountService;
-
 @RestController
 @RequestMapping("/api/account")
 public class AccountController {
 
-    private final AccountService accountService;
-
-    public AccountController() {
-    }
 }
