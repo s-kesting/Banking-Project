368f67222c5d25d950f3ee7c28eb038087a3e4cc
ZFengLiang <712789@student.inholland.nl>
2025-05-27 17:21:52 +0200

    MinorUpdate
    small changes from the monday

--- Front-End/src/config.js
+++ Front-End/src/config.js
@@ -1,12 +1,12 @@
 export const API_BASE_URL = "http://localhost:8080/api/";
 
 export const API_ENDPOINTS = {
-  auth: `${API_BASE_URL}user/auth/`,
-  users: `${API_BASE_URL}user/`,
-    register: `${API_BASE_URL}user/auth/register/`,
-    login: `${API_BASE_URL}/user/auth/login/`,
-  accounts: `${API_BASE_URL}accounts/`,
-  transactions: `${API_BASE_URL}transactions/`,
+  auth: `${API_BASE_URL}user/auth`,
+  users: `${API_BASE_URL}user`,
+    register: `${API_BASE_URL}user/auth/register`,
+    login: `${API_BASE_URL}user/auth/login`,
+  accounts: `${API_BASE_URL}accounts`,
+  transactions: `${API_BASE_URL}transactions`,
 
 };
 export default API_ENDPOINTS;
