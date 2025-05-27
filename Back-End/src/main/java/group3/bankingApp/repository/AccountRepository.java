adfa73ec74131dd92447e3597277ff16681acd6e
ZFengLiang <712789@student.inholland.nl>
2025-05-27 20:01:01 +0200

    AddedBackEndTransaction
    added the backend for transaction

--- Back-End/src/main/java/group3/bankingApp/repository/AccountRepository.java
+++ Back-End/src/main/java/group3/bankingApp/repository/AccountRepository.java
@@ -3,10 +3,17 @@ package group3.bankingApp.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import group3.bankingApp.model.Account;
+
+import java.util.List;
+
+import com.fasterxml.jackson.databind.deser.impl.CreatorCandidate;
+
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Integer> {
@@ -15,4 +22,13 @@ public interface AccountRepository extends JpaRepository<Account, Integer> {
     // a.ABSOLUTE_LIMIT, a.VERIFY_ACCOUNT, a.ACCOUNT_TYPE FROM ACCOUNT AS a WHERE
     // USERID = :userId")
     Optional<Account> findByUserId(int userId);
+    
+    
+    @Modifying
+    @Query("Update Account a SET a.balance = a.balance - :amount WHERE a.accountId =:id")
+    void withdraw(@Param("id") Integer id, @Param("amount") double amount);
+
+    @Modifying
+    @Query("Update Account a SET a.balance = a.balance + :amount WHRE a.account =:id")
+    void deposite(@Param("id") Integer id, @Param("amount") double amount);
 }
--- Back-End/src/main/java/group3/bankingApp/services/TransactionService.java
+++ Back-End/src/main/java/group3/bankingApp/services/TransactionService.java
@@ -2,14 +2,36 @@ package group3.bankingApp.services;
 
 import org.springframework.stereotype.Service;
 
+import group3.bankingApp.model.Transaction;
+import group3.bankingApp.repository.AccountRepository;
 import group3.bankingApp.repository.TransactionRepository;
+import jakarta.transaction.Transactional;
 
 @Service
 public class TransactionService {
     private final TransactionRepository transactionRepository;
+    private final AccountRepository accountRepository;
 
-    public TransactionService(TransactionRepository transactionRepository) {
+    public TransactionService(AccountRepository accountRepository,TransactionRepository transactionRepository){
+        this.accountRepository = accountRepository;
         this.transactionRepository = transactionRepository;
     }
+
+    public Transaction save(Transaction transaction){
+        return transactionRepository.save(transaction);
+    }
+
+    @Transactional
+    public Transaction CreateTransaction(Transaction transaction){
+        //sender
+        accountRepository.withdraw(transaction.getSenderAccount(), transaction.getAmount());
+        //receiver
+        accountRepository.deposite(transaction.getReceiverAccount(),transaction.getAmount());
+        //save transaction record
+        return transactionRepository.save(transaction);
+        
+    }
+
+
 
 }
