b8d4604eb1306e7b3a38cad5d8c6afa5dddf5c5a
ZFengLiang <712789@student.inholland.nl>
2025-06-12 15:05:25 +0200

    update

--- Front-End/src/config.js
+++ Front-End/src/config.js
@@ -8,7 +8,6 @@ export const API_ENDPOINTS = {
     login: `${API_BASE_URL}user/auth/login`,
 
     // accounts
-    account: `${API_BASE_URL}account`,
     userAccounts: `${API_BASE_URL}account/user`,
     userSavingsAccounts: `${API_BASE_URL}account/user/savings`,
     userCheckingsAccounts: `${API_BASE_URL}account/user/checkings`,
@@ -22,16 +21,11 @@ export const API_ENDPOINTS = {
 
     //users
     users: `${API_BASE_URL}user`,
-    auth: `${API_BASE_URL}user/auth`,
-    users: `${API_BASE_URL}user`,
     account: `${API_BASE_URL}account`,
     userAccounts: `${API_BASE_URL}account/user`,
-    transactions: `${API_BASE_URL}transactions`,
     employee: `${API_BASE_URL}employee`,
 
     //Authentication
-    register: `${API_BASE_URL}user/auth/register`,
-    login: `${API_BASE_URL}user/auth/login`,
     checkUsername: `${API_BASE_URL}user/auth/check-username`,
     checkEmail: `${API_BASE_URL}user/auth/check-email`,
     checkBsn: `${API_BASE_URL}user/auth/check-bsn`,
