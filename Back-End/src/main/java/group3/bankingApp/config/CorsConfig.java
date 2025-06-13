494833bf8740f4849eefd74e12946b4042d61200
ZFengLiang <144345368+ZFengLiang@users.noreply.github.com>
2025-06-13 13:34:58 +0200

    Update CorsConfig.java

--- Back-End/src/main/java/group3/bankingApp/config/CorsConfig.java
+++ Back-End/src/main/java/group3/bankingApp/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://dancing-marigold-02755e.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
